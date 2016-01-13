021b9fbebc7c459a3139566da38b6c8850081768
mgperry <mgperry32@gmail.com>
2016-01-13 15:00:15 +0000

    pwm functionality added to getPatternOccurrence

--- R/MotifScanningFunctions.R
+++ R/MotifScanningFunctions.R
@@ -1,25 +1,3 @@
-.scan.sequence.with.pwm <- function(pwm, seq, minScore){
-    
-    nc <- nchar(minScore)
-    if (substr(minScore, nc, nc) == "%"){
-        perc.threshold <- substr(minScore, 1L, nc-1L)
-        min.score <- minScore(pwm)
-        max.score <- maxScore(pwm)
-        score.threshold = min.score + as.double(perc.threshold)/100 *
-            (max.score-min.score)
-    }else{
-        score.threshold <- minScore
-    }
-
-    # make all PWM scores positive to avoid matching the Ns which are assigned score 0
-    pwm.corr <- pwm + abs(min(pwm))
-    score.threshold <- score.threshold + ncol(pwm) * abs(min(pwm))
-
-    pwm.match <- matchPWM(pwm = pwm.corr, subject = seq, min.score = score.threshold)
-    return(start(pwm.match))
-
-}
-
 .get.scanning.score <- function(pwm, seq){
 
 # make all PWM scores positive to avoid matching the Ns which are assigned score 0
--- R/PatternOccurrenceMethods.R
+++ R/PatternOccurrenceMethods.R
@@ -9,19 +9,41 @@ setMethod("getPatternOccurrence",
 signature(seq = "DNAStringSet", pattern="character"),
     function(seq, pattern) {
 
-        if(!(length(unique(width(seq))) == 1)){
+        if (!(length(unique(width(seq))) == 1)) {
             stop("All sequences in the input DNAStringSet must have the same
             length!")
         }
 
-        if(length(pattern) != 1){
+        if (length(pattern) != 1) {
             stop("Only one pattern must to be specified!")
         }
 
-        seq <- DNAStringSet(gsub("N", "+", seq))
+        seq <- DNAStringSet(gsub("N", "+", seq)) # avoid matching Ns
 
         pattern.matches <- vmatchPattern(pattern=pattern, subject=seq, fixed=FALSE)
         pattern.starts <- startIndex(pattern.matches)
+        pattern.starts.ul <- unlist(pattern.starts)
+        sm = sparseMatrix(
+            i = rep(1:length(pattern.starts), lengths(pattern.starts)),
+            j = pattern.starts.ul,
+            dims = c(length(seq), width(seq)[1]))
+        return(sm)
+    }
+)
+
+setMethod("getPatternOccurrence",
+signature(regionsSeq = "DNAStringSet", pattern = "matrix"),
+function(regionsSeq, pattern, min.score = "80%") {
+
+        if (!(length(unique(width(regionsSeq))) == 1)) {
+            stop("All sequences in the input DNAStringSet must have the same
+            length!")
+        }
+
+        # Are Ns a problem?
+
+        pattern.matches <- matchPWM(pwm=pattern, subject=seq, min.score=min.score)
+        pattern.starts <- start(pattern.matches)
         pattern.starts.ul <- unlist(pattern.starts)
         sm = sparseMatrix(
             i = rep(1:length(pattern.starts), lengths(pattern.starts)),
