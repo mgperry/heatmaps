c9d4aed3d38b05ac42f14224791a979cbc34e581
mgperry <mgperry32@gmail.com>
2016-01-13 15:13:31 +0000

    tidied files

--- R/Heatmap-plotting-methods.R
+++ R/Heatmap-plotting-methods.R
@@ -1,8 +1,5 @@
-# should be generic called plotHeatmap or similar
-ss_heatmap <- function(
-        heatmap,
-        options=NULL,
-        ...) {
+setMethod("plot", signature="Heatmap",
+    function(heatmap, options=NULL, ...) {
 
     if (is.null(options)) {
         options = heatmapOptions(...)
@@ -76,5 +73,49 @@ heatmapOptions = function(...) {
         addReferenceLine=TRUE)
     def[names(usr)] = usr
     return(def)
+}
+
+# additional functions
+
+plot_legend <- function(max_value, untransf, color='blue', cex=8) {
+        f <- .myColorPalette(color)
+        nr.labels <- 4
+        leg <- rep('', 256)
+        leg[seq(0.05*256, 0.95*256, length.out=nr.labels)] <-
+            formatC(untransf(seq(0.05*max_value, 0.95*max_value, length.out=nr.labels)),
+                    format='f', digits=2)
+        par(mar = c(12, 14, 2, 0.5))
+        plot(1, 1, type='n', bty='n', xaxt='n', yaxt='n', xlim=c(0,1),
+        ylim=c(0,7), xaxs="i", yaxs="i", xlab='', ylab='')
+        box(lwd = 6)
+        color.legend(0, 0, 1, 7, legend=leg, rect.col=f(256), align="lt", gradient='y', cex=cex)
+}
+
+.myColorPalette <- function(colorName){
+    colnames = c("green", "cyan", "blue", "purple", "pink", "red", "orange", "brown", "gray")
+        if(!(colorName %in% colnames)){
+            msg = paste("Colour", colorName, "not supported, must be one of", paste(colnames, collapse=', '))
+            stop(msg)
+        }
+    colors.df <- data.frame(
+        midcol=c("palegreen", "aquamarine", "lightblue", "lavender",
+                 "mistyrose", "peachpuff", "lightgoldenrod2", "wheat2", "lightgray"),
+        highcol=c("darkgreen", "darkslategrey", "blue4", "purple4",
+                  "deeppink4", "red4", "darkorange4", "salmon4", "black"),
+        stringsAsFactors = FALSE)
+    rownames(colors.df) <- colnames
+    colorRampPalette( c("white", colors.df[colorName,"midcol"], colors.df[colorName,"highcol"]) )
+}
+
+log5 <- function(x) {
+    log10(x)/log10(5)
+}
+
+make_x_ticks = function(coord) {
+    xTicks <- c(1*coord[1], 1*coord[1]/2, 0, coord[2]/2, coord[2])
+    xTicksAt <- cumsum(c(0.5, -coord[1]/2, -coord[1]/2, coord[2]/2-1,
+        coord[2]/2))
+    names(xTicksAt) = xTicks
+    return(xTicksAt)
 }
 
--- R/MotifScanningMethods.R
+++ R/MotifScanningMethods.R
@@ -39,3 +39,16 @@ function(regionsSeq, motifPWM, seqOrder = c(1:length(regionsSeq)),
     }
 )
 
+.get.scanning.score <- function(pwm, seq){
+
+# make all PWM scores positive to avoid matching the Ns which are assigned score 0
+    pwm.corr <- pwm + abs(min(pwm))
+
+    pwm.scores <- PWMscoreStartingAt(pwm = pwm.corr, subject = seq,
+    starting.at = c(1:(length(seq) - ncol(pwm) + 1)))
+
+    pwm.scores <- pwm.scores - ncol(pwm) * abs(min(pwm))
+    return(pwm.scores)
+
+}
+
