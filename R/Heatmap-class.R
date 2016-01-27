008b7620b5dd31e32976c49af95cb0f13c898616
mgperry <mgperry32@gmail.com>
2016-01-27 16:43:44 +0000

    added new Heatmap methods + constructor

--- R/Heatmap-class.R
+++ R/Heatmap-class.R
@@ -11,7 +11,7 @@ setClass("Heatmap",
         prototype=list(
             xm=numeric(0),
             ym=numeric(0),
-            value=matrix(0, ncol=0, nrow=0),
+            matrix=matrix(0, ncol=0, nrow=0),
             max_value=0,
             coords=c(0L,0L),
             nseq=0L,
@@ -30,4 +30,44 @@ setMethod("show", signature="Heatmap", function(object) {
     cat("max_value:", object@max_value, "\n")
     cat("label:", object@label, "\n")
 })
+
+setGeneric("mirror", def=function(x) standardGeneric("mirror"))
+
+setMethod("mirror", signature="Heatmap", function(x) {
+    x@matrix = t(apply(x@matrix, 1, rev))
+    x@coords = -rev(x@coords)
+    x
+})
+
+setMethod("rev", signature="Heatmap", function(x) {
+    x@matrix = apply(x@matrix, 2, rev)
+    x
+})
+
+Heatmap = function(mat, coords=NULL, label="", nseq=NULL, max_value=max(mat), metadata=list()) {
+    if (is.null(coords)) {
+        coords = c(0L, ncol(mat))
+    } else {
+        coords = as.integer(coords)
+    }
+    if (is.null(nseq)) {
+        nseq = nrow(mat)
+    } else {
+        nseq = as.integer(nseq)
+    }
+    xm = seq(1, coords[2] - coords[1], length.out=ncol(mat))
+    ym = 1:nrow(mat)
+    hm = new(
+        "Heatmap",
+        xm=xm,
+        ym=ym,
+        matrix=mat,
+        max_value=max_value,
+        coords=coords,
+        nseq=nseq,
+        label=label,
+        metadata=metadata)
+    hm
+}
+
 
