7eba518cfa37eac92ad73ed63a3aeb451406afe2
mgperry <mgperry32@gmail.com>
2016-09-16 17:50:25 +0100

    docs for Heatmap-class

--- R/Heatmap-class.R
+++ R/Heatmap-class.R
@@ -1,13 +1,27 @@
+#' An S4 class to represent a heatmap
+#'
+#' @slot image A numeric Matrix
+#' @slot scale A length-2 vector
+#' @slot coords A length-2 integer vector
+#' @slot nseq An integer
+#' @slot label A character vector
+#' @slot metadata A list containing arbitrary metadata
+#'
+#' A class used to represent a heatmap in a simple, self-contained way
+#'
+#' @export
+#'
+#' @seealso CoverageHeatmap PatternHeatmap plotHeatmap plotHeatmapMeta
 setClass("Heatmap",
          slots=c(
-            matrix="matrix",
+            image="matrix",
             scale="numeric",
             coords="integer",
             nseq="integer",
             label="character",
             metadata="list"),
         prototype=list(
-            matrix=matrix(0, ncol=0, nrow=0),
+            image=matrix(0, ncol=0, nrow=0),
             scale=c(0,0),
             coords=c(0L,0L),
             nseq=0L,
@@ -15,8 +29,16 @@ setClass("Heatmap",
             metadata=list()),
 )
 
+#' @describeIn Heatmap Return the number of sequences in a heatmap
+#' @export
 setMethod("length", signature="Heatmap", function(x) x@nseq)
 
+#' @importMethodsFrom BiocGenerics width
+NULL
+
+#' @describeIn Heatmap Return the width of sequence represented in a heatmap
+#' @importMethodsFrom BiocGenerics width
+#' @export
 setMethod("width", signature="Heatmap", function(x) x@coords[2] - x@coords[1])
 
 setMethod("show", signature="Heatmap", function(object) {
@@ -27,55 +49,112 @@ setMethod("show", signature="Heatmap", function(object) {
     cat("label:", object@label, "\n")
 })
 
+#' Reflect a heatmap in the y axis
+#'
+#' @param x A heatmap
+#' @export
 setGeneric("mirror", def=function(x) standardGeneric("mirror"))
 
+#' @describeIn mirror Heatmap method
+#' @export
 setMethod("mirror", signature="Heatmap", function(x) {
     image(x) = t(apply(image(x), 1, rev))
     x@coords = -rev(x@coords)
     x
 })
 
+#' Reflect a heatmap in the x axis
+#'
+#' @param x A heatmap
+#' @export
 setMethod("rev", signature="Heatmap", function(x) {
     image(x) = apply(image(x), 2, rev)
     x
 })
 
+#' Generate co-ordinates for each row of the image matrix of a Heatmap
+#' @param x A Heatmap
+#' @export
 setGeneric("xm", def=function(x) standardGeneric("xm"))
 
+#' @describeIn xm Generate co-ordinates for each frow of the image matrix of a Heatmap
+#' @export
 setMethod("xm", signature="Heatmap", function(x) {
     seq(1, width(x), length.out=ncol(image(x)))
 })
 
+#' Generate co-ordinates for each column of the image matrix of a Heatmap
+#' @param x A Heatmap
+#' @export
 setGeneric("ym", def=function(x) standardGeneric("ym"))
 
+#' @describeIn ym Generate co-ordinates for each column of the matrix
+#' @export
 setMethod("ym", signature="Heatmap", function(x) {
     seq(1, x@nseq, length.out=nrow(image(x)))
 })
 
+#' Return or set the scale in a Heatmap
+#' @name scale
+NULL
+
+#' @rdname scale
+#' @export
 setGeneric("scale", def=function(x) standardGeneric("scale"))
 
+#' @rdname scale
+#' @export
 setMethod("scale", signature="Heatmap", function(x) {
     x@scale
 })
 
+#' @rdname scale
+#' @export
 setGeneric("scale<-", def=function(x, value, ...) standardGeneric("scale<-"))
 
+#' @rdname scale
+#' @export
 setMethod("scale<-", signature="Heatmap", function(x, value) {
     x@scale = value
     x
 })
 
+#' Return or set the image in a Heatmap
+#' @name image
+NULL
+
+#' @rdname image
+#' @export
 setMethod("image", signature="Heatmap", function(x) {
     x@matrix
 })
 
+#' @rdname image
+#' @export
 setGeneric("image<-", def=function(x, value, ...) standardGeneric("image<-"))
 
+#' @rdname image
+#' @export
 setMethod("image<-", signature="Heatmap", function(x, value) {
     x@matrix = value
     x
 })
 
+#' Function to create a heatmap object
+#'
+#' @param image A numeric Matrix
+#' @param scale A length-2 vector
+#' @param coords A length-2 integer vector
+#' @param nseq An integer
+#' @param label A character vector
+#' @param metadata A list containing arbitrary metadata
+#'
+#' Using this function avoids calling 'new' directly or manually setting coords and
+#' nseq to integers. Other constructors exist for creating heatmaps from data, rather
+#' than a raw matrix.
+#'
+#' @seealso PatternHeatmap CoverageHeatmap PWMScanHeatmap
+#' @export
 Heatmap = function(mat, coords=NULL, label="", nseq=NULL, scale=max(mat), metadata=list()) {
     if (is.null(coords)) {
         coords = c(0L, ncol(mat))
