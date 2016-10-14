bbb9fe335876416557d4ca76e69aa7bdae58461a
mgperry <mgperry32@gmail.com>
2016-10-14 11:53:45 +0100

    fixed odd behaviour with binned heatmaps

--- R/plotHeatmapMeta.R
+++ R/plotHeatmapMeta.R
@@ -23,13 +23,31 @@ plotHeatmapMeta = function(hm_list, binsize=1, colors=gg_col(length(hm_list)), a
     if (!length(unique(lapply(hm_list, function(x) x@coords))) == 1)
         stop("heatmaps must have the same coordinates")
 
-    if (!length(unique(lapply(hm_list, function(x) x@coords))) == 1)
+    co_ord = coords(hm_list[[1]])
+
+    if (!length(unique(lapply(hm_list, function(x) xm(x)))) == 1)
+        stop("heatmaps must have the same xm values")
+
+    n_seq = unique(sapply(hm_list, function(x) x@nseq))
+
+    if (!length(n_seq) == 1)
         stop("heatmaps must have the same number of sequences")
 
     coords = hm_list[[1]]@coords
-    breaks = seq(0, width(hm_list[[1]]), by=binsize)
-    bin_sums = lapply(hm_list, bin_heatmap, breaks=breaks)
-    occurrence = lapply(bin_sums, function(x) x/(hm_list[[1]]@nseq*binsize))
+    if (binsize != 1) {
+        if (!all(xm(hm_list[[1]]) == 1:width(hm_list[[1]]))) {
+            stop("cannot set binsize for heatmaps which are already binned/smoothed")
+        }
+        breaks = seq(0, width(hm_list[[1]]), by=binsize)
+        bin_sums = lapply(hm_list, bin_heatmap, breaks=breaks)
+        x_coord = breaks[1:(length(breaks)-1)] + binsize/2 + coords[1]
+    } else {
+        breaks = xm(hm_list[[1]])
+        x_coord = breaks + binsize/2 + coords[1]
+        bin_sums = lapply(hm_list, function(x) colSums(image(x)))
+    }
+    scale_factor = n_seq*(width(hm_list[[1]])/length(x_coord))
+    occurrence = lapply(bin_sums, function(x) x/scale_factor)
     max_value = max(vapply(occurrence, max, numeric(1)))
     plot(0, 0, xlim=coords, ylim=c(0, max_value), axes=FALSE, type="n", xlab="", ylab="")
     axis(1)
@@ -37,7 +55,6 @@ plotHeatmapMeta = function(hm_list, binsize=1, colors=gg_col(length(hm_list)), a
     mtext("Relative Position", side = 1, line = 3, cex = 1.5, font = 2)
     mtext("Frequency", side = 2, line = 2.5, cex = 1.5, font = 2)
     for (i in seq_along(occurrence)) {
-        x_coord = breaks[1:(length(breaks)-1)] + binsize/2 + coords[1]
         lines(x_coord, occurrence[[i]], col = colors[i], type='l', lwd=2)
     }
     labels = vapply(hm_list, function(x) x@label, character(1))
